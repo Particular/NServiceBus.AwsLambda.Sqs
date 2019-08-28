934d2148426b532279fe6717ebc2da05b5213ef2
danielmarbach <daniel.marbach@openplace.net>
2019-08-28 11:04:06 +0000

    Removed unneeded settings

--- src/NServiceBus.AwsLambda.SQS/SettingsKeys.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NServiceBus
-{
-    static class SettingsKeys
-    {
-        const string Prefix = "NServiceBus.AmazonSQS.";
-        public const string S3BucketForLargeMessages = Prefix + nameof(S3BucketForLargeMessages);
-    }
-}
