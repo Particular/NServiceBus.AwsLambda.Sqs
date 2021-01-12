d5c89f6353f4098014aa7de8654616ab527f9314
Sean Feldman <feldman.sean@gmail.com>
2021-01-11 17:29:23 -0700

    Introduce support for operations outside of message handler

--- src/NServiceBus.AwsLambda.SQS/IAwsLambdaSQSEndpoint.cs
+++ src/NServiceBus.AwsLambda.SQS/IAwsLambdaSQSEndpoint.cs
@@ -1,4 +1,6 @@
-﻿namespace NServiceBus
+﻿using System;
+
+namespace NServiceBus
 {
     using System.Threading;
     using System.Threading.Tasks;
@@ -15,5 +17,68 @@
         /// Processes a messages received from an SQS trigger using the NServiceBus message pipeline.
         /// </summary>
         Task Process(SQSEvent @event, ILambdaContext lambdaContext, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Sends the provided message.
+        /// </summary>
+        Task Send(object message, SendOptions options, ILambdaContext lambdaContext);
+
+        /// <summary>
+        /// Sends the provided message.
+        /// </summary>
+        Task Send(object message, ILambdaContext lambdaContext);
+
+        /// <summary>
+        /// Instantiates a message of type T and sends it.
+        /// </summary>
+        Task Send<T>(Action<T> messageConstructor, SendOptions options, ILambdaContext lambdaContext);
+
+        /// <summary>
+        /// Instantiates a message of type T and sends it.
+        /// </summary>
+        Task Send<T>(Action<T> messageConstructor, ILambdaContext lambdaContext);
+
+        /// <summary>
+        /// Publish the message to subscribers.
+        /// </summary>
+        Task Publish(object message, PublishOptions options, ILambdaContext lambdaContext);
+
+        /// <summary>
+        /// Instantiates a message of type T and publishes it.
+        /// </summary>
+        Task Publish<T>(Action<T> messageConstructor, PublishOptions options, ILambdaContext lambdaContext);
+
+        /// <summary>
+        /// Instantiates a message of type T and publishes it.
+        /// </summary>
+        Task Publish(object message, ILambdaContext lambdaContext);
+
+        /// <summary>
+        /// Instantiates a message of type T and publishes it.
+        /// </summary>
+        Task Publish<T>(Action<T> messageConstructor, ILambdaContext lambdaContext);
+
+        /// <summary>
+        /// Subscribes to receive published messages of the specified type.
+        /// This method is only necessary if you turned off auto-subscribe.
+        /// </summary>
+        Task Subscribe(Type eventType, SubscribeOptions options, ILambdaContext lambdaContext);
+
+        /// <summary>
+        /// Subscribes to receive published messages of the specified type.
+        /// This method is only necessary if you turned off auto-subscribe.
+        /// </summary>
+        Task Subscribe(Type eventType, ILambdaContext lambdaContext);
+
+        /// <summary>
+        /// Unsubscribes to receive published messages of the specified type.
+        /// </summary>
+        Task Unsubscribe(Type eventType, UnsubscribeOptions options, ILambdaContext lambdaContext);
+
+        /// <summary>
+        /// Unsubscribes to receive published messages of the specified type.
+        /// </summary>
+        Task Unsubscribe(Type eventType, ILambdaContext lambdaContext);
+
     }
 }
