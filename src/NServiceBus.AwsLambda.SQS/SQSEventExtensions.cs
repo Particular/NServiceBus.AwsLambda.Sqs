8846014c04d0beebd4ca18c141ed3d2851ea4281
kentdr <dan.kent@particular.net>
2023-04-14 15:04:38 -0400

    Added too many if statements to check for null

--- src/NServiceBus.AwsLambda.SQS/SQSEventExtensions.cs
+++ src/NServiceBus.AwsLambda.SQS/SQSEventExtensions.cs
@@ -2,38 +2,94 @@
 {
     using System.Collections.Generic;
     using System.IO;
-    using System.Linq;
-    using Amazon.Lambda.SQSEvents;
     using Amazon.SQS.Model;
+    using static Amazon.Lambda.SQSEvents.SQSEvent;
 
     static class SQSEventExtensions
     {
-        public static Message ToMessage(this SQSEvent.SQSMessage sqsEventRecord)
+        public static Message ToMessage(this SQSMessage sqsEventRecord)
         {
-            return new Message()
+            var newMessage = new Message();
+
+            if (!string.IsNullOrEmpty(sqsEventRecord.MessageId))
             {
-                MessageId = sqsEventRecord.MessageId,
-                ReceiptHandle = sqsEventRecord.ReceiptHandle,
-                Attributes = sqsEventRecord.Attributes,
-                Body = sqsEventRecord.Body,
-                MD5OfBody = sqsEventRecord.Md5OfBody,
-                MD5OfMessageAttributes = sqsEventRecord.Md5OfMessageAttributes,
-                MessageAttributes = ToMessageAttributes(sqsEventRecord.MessageAttributes)
-            };
+                newMessage.MessageId = sqsEventRecord.MessageId;
+            }
+
+            if (!string.IsNullOrEmpty(sqsEventRecord.ReceiptHandle))
+            {
+                newMessage.ReceiptHandle = sqsEventRecord.ReceiptHandle;
+            }
+
+            if (sqsEventRecord.Attributes != null)
+            {
+                newMessage.Attributes = sqsEventRecord.Attributes;
+            }
+
+            if (!string.IsNullOrEmpty(sqsEventRecord.Body))
+            {
+                newMessage.Body = sqsEventRecord.Body;
+            }
+
+            if (!string.IsNullOrEmpty(sqsEventRecord.Md5OfBody))
+            {
+                newMessage.MD5OfBody = sqsEventRecord.Md5OfBody;
+            }
+
+            if (!string.IsNullOrEmpty(sqsEventRecord.Md5OfMessageAttributes))
+            {
+                newMessage.MD5OfMessageAttributes = sqsEventRecord.Md5OfMessageAttributes;
+            }
+
+            if (!string.IsNullOrEmpty(sqsEventRecord.Md5OfMessageAttributes))
+            {
+                newMessage.MD5OfMessageAttributes = sqsEventRecord.Md5OfMessageAttributes;
+            }
+
+            if (sqsEventRecord.MessageAttributes != null)
+            {
+                newMessage.MessageAttributes = ToMessageAttributes(sqsEventRecord.MessageAttributes);
+            }
+
+            return newMessage;
         }
 
-        static Dictionary<string, MessageAttributeValue> ToMessageAttributes(Dictionary<string, SQSEvent.MessageAttribute> messageAttributes)
+        static Dictionary<string, MessageAttributeValue> ToMessageAttributes(Dictionary<string, MessageAttribute> messageAttributes)
         {
-            return new Dictionary<string, MessageAttributeValue>(messageAttributes
-            .Select(s => new KeyValuePair<string, MessageAttributeValue>(s.Key, new MessageAttributeValue()
+            var newMessageAttributes = new Dictionary<string, MessageAttributeValue>(messageAttributes.Count);
+
+            foreach (var attribute in messageAttributes)
             {
-                DataType = s.Value.DataType,
-                BinaryValue = s.Value.BinaryValue,
-                StringValue = s.Value.StringValue,
-                // The SQS client returns empty lists instead of null
-                StringListValues = s.Value.StringListValues ?? new List<string>(),
-                BinaryListValues = s.Value.BinaryListValues ?? new List<MemoryStream>(),
-            })));
+                newMessageAttributes.Add(attribute.Key, CopyMessageAttributeValue(attribute.Value));
+            }
+
+            return newMessageAttributes;
+        }
+
+        static MessageAttributeValue CopyMessageAttributeValue(MessageAttribute valueToCopy)
+        {
+            var newValue = new MessageAttributeValue();
+
+            if (!string.IsNullOrEmpty(valueToCopy.DataType))
+            {
+                newValue.DataType = valueToCopy.DataType;
+            }
+
+            if (!string.IsNullOrEmpty(valueToCopy.StringValue))
+            {
+                newValue.StringValue = valueToCopy.StringValue;
+            }
+
+            if (valueToCopy.BinaryValue != null)
+            {
+                newValue.BinaryValue = valueToCopy.BinaryValue;
+            }
+
+            // The SQS client returns empty lists instead of null
+            newValue.StringListValues = valueToCopy.StringListValues ?? new List<string>(0);
+            newValue.BinaryListValues = valueToCopy.BinaryListValues ?? new List<MemoryStream>(0);
+
+            return newValue;
         }
     }
 }
