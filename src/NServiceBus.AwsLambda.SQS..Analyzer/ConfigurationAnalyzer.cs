c6a89969cd326c4bf629ea53f49f95d6a561c572
Jo Palac <j_palac@hotmail.com>
2023-09-15 14:39:01 +1000

    Added Rosylyn analyzer tests for not allowed api calls

---
+++ src/NServiceBus.AwsLambda.SQS..Analyzer/ConfigurationAnalyzer.cs
@@ -0,0 +1,167 @@
+namespace NServiceBus.AwsLambda.SQS.Analyzer
+{
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using NServiceBus.AwsLambda.SQS.Analyzer.Extensions;
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ConfigurationAnalyzer : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(
+            AwsLambdaDiagnostics.PurgeOnStartupNotAllowed,
+            AwsLambdaDiagnostics.LimitMessageProcessingToNotAllowed,
+            AwsLambdaDiagnostics.DefineCriticalErrorActionNotAllowed,
+            AwsLambdaDiagnostics.SetDiagnosticsPathNotAllowed,
+            AwsLambdaDiagnostics.MakeInstanceUniquelyAddressableNotAllowed,
+            AwsLambdaDiagnostics.UseTransportNotAllowed,
+            AwsLambdaDiagnostics.OverrideLocalAddressNotAllowed,
+            AwsLambdaDiagnostics.RouteReplyToThisInstanceNotAllowed,
+            AwsLambdaDiagnostics.RouteToThisInstanceNotAllowed,
+            AwsLambdaDiagnostics.TransportTransactionModeNotAllowed
+        );
+
+        static readonly Dictionary<string, DiagnosticDescriptor> NotAllowedEndpointConfigurationMethods
+            = new Dictionary<string, DiagnosticDescriptor>
+            {
+                ["PurgeOnStartup"] = AwsLambdaDiagnostics.PurgeOnStartupNotAllowed,
+                ["LimitMessageProcessingConcurrencyTo"] = AwsLambdaDiagnostics.LimitMessageProcessingToNotAllowed,
+                ["DefineCriticalErrorAction"] = AwsLambdaDiagnostics.DefineCriticalErrorActionNotAllowed,
+                ["SetDiagnosticsPath"] = AwsLambdaDiagnostics.SetDiagnosticsPathNotAllowed,
+                ["MakeInstanceUniquelyAddressable"] = AwsLambdaDiagnostics.MakeInstanceUniquelyAddressableNotAllowed,
+                ["UseTransport"] = AwsLambdaDiagnostics.UseTransportNotAllowed,
+                ["OverrideLocalAddress"] = AwsLambdaDiagnostics.OverrideLocalAddressNotAllowed,
+            };
+
+        static readonly Dictionary<string, DiagnosticDescriptor> NotAllowedSendAndReplyOptions
+            = new Dictionary<string, DiagnosticDescriptor>
+            {
+                ["RouteReplyToThisInstance"] = AwsLambdaDiagnostics.RouteReplyToThisInstanceNotAllowed,
+                ["RouteToThisInstance"] = AwsLambdaDiagnostics.RouteToThisInstanceNotAllowed,
+            };
+
+        static readonly Dictionary<string, DiagnosticDescriptor> NotAllowedTransportSettings
+            = new Dictionary<string, DiagnosticDescriptor>
+            {
+                ["TransportTransactionMode"] = AwsLambdaDiagnostics.TransportTransactionModeNotAllowed,
+                ["Transactions"] = AwsLambdaDiagnostics.TransportTransactionModeNotAllowed
+            };
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.InvocationExpression);
+            context.RegisterSyntaxNodeAction(AnalyzeTransport, SyntaxKind.SimpleMemberAccessExpression);
+        }
+
+        static void Analyze(SyntaxNodeAnalysisContext context)
+        {
+            if (!(context.Node is InvocationExpressionSyntax invocationExpression))
+            {
+                return;
+            }
+
+            if (!(invocationExpression.Expression is MemberAccessExpressionSyntax memberAccessExpression))
+            {
+                return;
+            }
+
+            AnalyzeEndpointConfiguration(context, invocationExpression, memberAccessExpression);
+
+            AnalyzeSendAndReplyOptions(context, invocationExpression, memberAccessExpression);
+
+            AnalyzeTransportExtensions(context, invocationExpression, memberAccessExpression);
+        }
+
+        static void AnalyzeTransport(SyntaxNodeAnalysisContext context)
+        {
+            if (!(context.Node is MemberAccessExpressionSyntax memberAccess))
+            {
+                return;
+            }
+
+            if (!NotAllowedTransportSettings.TryGetValue(memberAccess.Name.ToString(), out var diagnosticDescriptor))
+            {
+                return;
+
+            }
+
+            var memberAccessSymbol = context.SemanticModel.GetSymbolInfo(memberAccess, context.CancellationToken);
+
+            if (!(memberAccessSymbol.Symbol is IPropertySymbol propertySymbol))
+            {
+                return;
+            }
+
+            if (propertySymbol.ContainingType.ToString() == "NServiceBus.SqsTransport" || propertySymbol.ContainingType.ToString() == "NServiceBus.Transport.TransportDefinition")
+            {
+                context.ReportDiagnostic(diagnosticDescriptor, memberAccess);
+
+            }
+        }
+
+        static void AnalyzeEndpointConfiguration(SyntaxNodeAnalysisContext context, InvocationExpressionSyntax invocationExpression, MemberAccessExpressionSyntax memberAccessExpression)
+        {
+            if (!NotAllowedEndpointConfigurationMethods.TryGetValue(memberAccessExpression.Name.Identifier.Text, out var diagnosticDescriptor))
+            {
+                return;
+            }
+
+            var memberAccessSymbol = context.SemanticModel.GetSymbolInfo(memberAccessExpression, context.CancellationToken);
+
+            if (!(memberAccessSymbol.Symbol is IMethodSymbol methodSymbol))
+            {
+                return;
+            }
+
+            if (methodSymbol.ReceiverType.ToString() == "NServiceBus.EndpointConfiguration")
+            {
+                context.ReportDiagnostic(diagnosticDescriptor, invocationExpression);
+            }
+        }
+
+        static void AnalyzeSendAndReplyOptions(SyntaxNodeAnalysisContext context, InvocationExpressionSyntax invocationExpression, MemberAccessExpressionSyntax memberAccessExpression)
+        {
+            if (!NotAllowedSendAndReplyOptions.TryGetValue(memberAccessExpression.Name.Identifier.Text, out var diagnosticDescriptor))
+            {
+                return;
+            }
+
+            var memberAccessSymbol = context.SemanticModel.GetSymbolInfo(memberAccessExpression, context.CancellationToken);
+
+            if (!(memberAccessSymbol.Symbol is IMethodSymbol methodSymbol))
+            {
+                return;
+            }
+
+            if (methodSymbol.ReceiverType.ToString() == "NServiceBus.SendOptions" || methodSymbol.ReceiverType.ToString() == "NServiceBus.ReplyOptions")
+            {
+                context.ReportDiagnostic(diagnosticDescriptor, invocationExpression);
+            }
+        }
+
+        static void AnalyzeTransportExtensions(SyntaxNodeAnalysisContext context, InvocationExpressionSyntax invocationExpression, MemberAccessExpressionSyntax memberAccessExpression)
+        {
+            if (!NotAllowedTransportSettings.TryGetValue(memberAccessExpression.Name.Identifier.Text, out var diagnosticDescriptor))
+            {
+                return;
+            }
+
+            var memberAccessSymbol = context.SemanticModel.GetSymbolInfo(memberAccessExpression, context.CancellationToken);
+
+            if (!(memberAccessSymbol.Symbol is IMethodSymbol methodSymbol))
+            {
+                return;
+            }
+
+            if (methodSymbol.ReceiverType.ToString() == "NServiceBus.TransportExtensions<NServiceBus.SqsTransport>")
+            {
+                context.ReportDiagnostic(diagnosticDescriptor, invocationExpression);
+            }
+        }
+    }
+}
