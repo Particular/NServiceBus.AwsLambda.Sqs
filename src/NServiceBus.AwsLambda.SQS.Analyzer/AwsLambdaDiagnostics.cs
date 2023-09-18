0fa5cc93fd1eb8669a0530d98111d73914193ff1
Jo Palac <j_palac@hotmail.com>
2023-09-18 12:01:42 +1000

    remove double . from naming and references to azure

--- src/NServiceBus.AwsLambda.SQS.Analyzer/AwsLambdaDiagnostics.cs
+++ src/NServiceBus.AwsLambda.SQS.Analyzer/AwsLambdaDiagnostics.cs
@@ -1,5 +1,6 @@
 ﻿namespace NServiceBus.AwsLambda.SQS.Analyzer
 {
+    using System.Security.Cryptography;
     using Microsoft.CodeAnalysis;
 
     public static class AwsLambdaDiagnostics
@@ -19,8 +20,8 @@
 
         internal static readonly DiagnosticDescriptor PurgeOnStartupNotAllowed = new DiagnosticDescriptor(
              id: PurgeOnStartupNotAllowedId,
-             title: "PurgeOnStartup is not supported in Azure Functions",
-             messageFormat: "Azure Functions endpoints do not support PurgeOnStartup.",
+             title: "PurgeOnStartup is not supported in Aws Lambda",
+             messageFormat: "Aws Lambda endpoints do not support PurgeOnStartup.",
              category: DiagnosticCategory,
              defaultSeverity: DiagnosticSeverity.Error,
              isEnabledByDefault: true
@@ -28,8 +29,8 @@
 
         internal static readonly DiagnosticDescriptor LimitMessageProcessingToNotAllowed = new DiagnosticDescriptor(
              id: LimitMessageProcessingToNotAllowedId,
-             title: "LimitMessageProcessing is not supported in Azure Functions",
-             messageFormat: "Concurrency-related settings are controlled via the Azure Function host.json configuration file.",
+             title: "LimitMessageProcessing is not supported in Aws Lambda",
+             messageFormat: "Concurrency-related settings can be configured with the Lambda API.",
              category: DiagnosticCategory,
              defaultSeverity: DiagnosticSeverity.Error,
              isEnabledByDefault: true
@@ -37,8 +38,8 @@
 
         internal static readonly DiagnosticDescriptor DefineCriticalErrorActionNotAllowed = new DiagnosticDescriptor(
              id: DefineCriticalErrorActionNotAllowedId,
-             title: "DefineCriticalErrorAction is not supported in Azure Functions",
-             messageFormat: "Azure Functions endpoints do not control the application lifecycle and should not define behavior in the case of critical errors.",
+             title: "DefineCriticalErrorAction is not supported in Aws Lambda",
+             messageFormat: "Aws Lambda endpoints do not control the application lifecycle and should not define behavior in the case of critical errors.",
              category: DiagnosticCategory,
              defaultSeverity: DiagnosticSeverity.Error,
              isEnabledByDefault: true
@@ -46,8 +47,8 @@
 
         internal static readonly DiagnosticDescriptor SetDiagnosticsPathNotAllowed = new DiagnosticDescriptor(
              id: SetDiagnosticsPathNotAllowedId,
-             title: "SetDiagnosticsPath is not supported in Azure Functions",
-             messageFormat: "Azure Functions endpoints should not write diagnostics to the local file system. Use CustomDiagnosticsWriter to write diagnostics to another location.",
+             title: "SetDiagnosticsPath is not supported in Aws Lambda",
+             messageFormat: "Aws Lambda endpoints should not write diagnostics to the local file system. Use CustomDiagnosticsWriter to write diagnostics to another location.",
              category: DiagnosticCategory,
              defaultSeverity: DiagnosticSeverity.Error,
              isEnabledByDefault: true
@@ -55,8 +56,8 @@
 
         internal static readonly DiagnosticDescriptor MakeInstanceUniquelyAddressableNotAllowed = new DiagnosticDescriptor(
              id: MakeInstanceUniquelyAddressableNotAllowedId,
-             title: "MakeInstanceUniquelyAddressable is not supported in Azure Functions",
-             messageFormat: "Azure Functions endpoints have unpredictable lifecycles and should not be uniquely addressable.",
+             title: "MakeInstanceUniquelyAddressable is not supported in Aws Lambda",
+             messageFormat: "Aws Lambda endpoints have unpredictable lifecycles and should not be uniquely addressable.",
              category: DiagnosticCategory,
              defaultSeverity: DiagnosticSeverity.Error,
              isEnabledByDefault: true
@@ -64,8 +65,8 @@
 
         internal static readonly DiagnosticDescriptor UseTransportNotAllowed = new DiagnosticDescriptor(
              id: UseTransportNotAllowedId,
-             title: "UseTransport is not supported in Azure Functions",
-             messageFormat: "The package configures Azure Service Bus transport by default. Use ServiceBusTriggeredEndpointConfiguration.Transport to access the transport configuration.",
+             title: "UseTransport is not supported in Aws Lambda",
+             messageFormat: "The package configures Amazon SQS transport by default. Use ServiceBusTriggeredEndpointConfiguration.Transport to access the transport configuration.",
              category: DiagnosticCategory,
              defaultSeverity: DiagnosticSeverity.Warning,
              isEnabledByDefault: true
@@ -73,8 +74,8 @@
 
         internal static readonly DiagnosticDescriptor OverrideLocalAddressNotAllowed = new DiagnosticDescriptor(
              id: OverrideLocalAddressNotAllowedId,
-             title: "OverrideLocalAddress is not supported in Azure Functions",
-             messageFormat: "The NServiceBus endpoint address in Azure Functions is determined by the ServiceBusTrigger attribute.",
+             title: "OverrideLocalAddress is not supported in Aws Lambda",
+             messageFormat: "The NServiceBus endpoint address in Aws Lambda is determined by the ServiceBusTrigger attribute.",
              category: DiagnosticCategory,
              defaultSeverity: DiagnosticSeverity.Error,
              isEnabledByDefault: true
@@ -82,8 +83,8 @@
 
         internal static readonly DiagnosticDescriptor RouteReplyToThisInstanceNotAllowed = new DiagnosticDescriptor(
              id: RouteReplyToThisInstanceNotAllowedId,
-             title: "RouteReplyToThisInstance is not supported in Azure Functions",
-             messageFormat: "Azure Functions instances cannot be directly addressed as they have a highly volatile lifetime. Use 'RouteToThisEndpoint' instead.",
+             title: "RouteReplyToThisInstance is not supported in Aws Lambda",
+             messageFormat: "Aws Lambda instances cannot be directly addressed as they have a highly volatile lifetime. Use 'RouteToThisEndpoint' instead.",
              category: DiagnosticCategory,
              defaultSeverity: DiagnosticSeverity.Error,
              isEnabledByDefault: true
@@ -91,8 +92,8 @@
 
         internal static readonly DiagnosticDescriptor RouteToThisInstanceNotAllowed = new DiagnosticDescriptor(
              id: RouteToThisInstanceNotAllowedId,
-             title: "RouteToThisInstance is not supported in Azure Functions",
-             messageFormat: "Azure Functions instances cannot be directly addressed as they have a highly volatile lifetime. Use 'RouteToThisEndpoint' instead.",
+             title: "RouteToThisInstance is not supported in Aws Lambda",
+             messageFormat: "Aws Lambda instances cannot be directly addressed as they have a highly volatile lifetime. Use 'RouteToThisEndpoint' instead.",
              category: DiagnosticCategory,
              defaultSeverity: DiagnosticSeverity.Error,
              isEnabledByDefault: true
@@ -100,8 +101,8 @@
 
         internal static readonly DiagnosticDescriptor TransportTransactionModeNotAllowed = new DiagnosticDescriptor(
              id: TransportTransactionModeNotAllowedId,
-             title: "TransportTransactionMode is not supported in Azure Functions",
-             messageFormat: "Transport TransactionMode is controlled by the Azure Service Bus trigger and cannot be configured via the NServiceBus transport configuration API when using Azure Functions.",
+             title: "TransportTransactionMode is not supported in Aws Lambda",
+             messageFormat: "Transport TransactionMode is controlled by the Aws Lambda trigger and cannot be configured via the NServiceBus transport configuration API when using Aws Lambda.",
              category: DiagnosticCategory,
              defaultSeverity: DiagnosticSeverity.Error,
              isEnabledByDefault: true
